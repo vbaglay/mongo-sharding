6f8beedbd99f74529efba7bf2d8bdb136c766e4f
Vyacheslav Baglay <terra-n3@yandex.ru>
2025-01-05 23:14:39 +0300

    fix exec

--- scripts/mongo-sharding.sh
+++ scripts/mongo-sharding.sh
@@ -2,7 +2,7 @@
 
 sudo docker-compose -p mongo-sharding up -d
 
-sudo  docker compose exec-T configSrv mongosh --port 27017 -quiet <<EOF
+sudo  docker compose exec -T configSrv mongosh --port 27017 -quiet <<EOF
 rs.initiate(
   {
     _id : "config_server",
@@ -15,7 +15,7 @@ rs.initiate(
 exit();
 EOF
 
-sudo docker compose exec-T shard1 mongosh --port 27018 -quiet <<EOF
+sudo docker compose exec -T shard1 mongosh --port 27018 -quiet <<EOF
 rs.initiate(
     {
       _id : "shard1",
@@ -27,7 +27,7 @@ rs.initiate(
 exit();
 EOF
 
-sudo docker compose exec-T shard2 mongosh --port 27019 -quiet <<EOF
+sudo docker compose exec -T shard2 mongosh --port 27019 -quiet <<EOF
 rs.initiate(
     {
       _id : "shard2",
@@ -39,7 +39,7 @@ rs.initiate(
 exit();
 EOF
 
-sudo docker compose exec-T mongos_router mongosh --port 27020 -quiet <<EOF
+sudo docker compose exec -T mongos_router mongosh --port 27020 -quiet <<EOF
 sh.addShard( "shard1/shard1:27018");
 sh.addShard( "shard2/shard2:27019");
 sh.enableSharding("somedb");
@@ -50,13 +50,13 @@ db.helloDoc.countDocuments()
 exit();
 EOF
 
-sudo docker compose exec-T shard1 mongosh --port 27018 -quiet <<EOF
+sudo docker compose exec -T shard1 mongosh --port 27018 -quiet <<EOF
 use somedb;
 db.helloDoc.countDocuments();
 exit();
 EOF
 
-sudo docker compose exec-T shard2 mongosh --port 27019 -quiet <<EOF
+sudo docker compose exec -T shard2 mongosh --port 27019 -quiet <<EOF
 use somedb;
 db.helloDoc.countDocuments();
 exit();
